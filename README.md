e3fd7f6459bf7326ef2a502e22dd8a2249f6f431
Hans Kokx <skipmeister123@gmail.com>
2013-11-02 12:04:21 -0400

    Renamed to gcwconnect

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-wificonfig
+gcwconnect
 ==========
 
 A simple wireless configuration manager for the GCW-Zero, written in Python and pygame.
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 chmod +x build/wificonfig.py
-rm wificonfig.opk
-mksquashfs build/* wificonfig.opk -noappend -comp gzip -all-root
-scp wificonfig.opk zero:/media/data/apps/
+rm gcwconnect.opk
+mksquashfs build/* gcwconnect.opk -noappend -comp gzip -all-root
+scp gcwconnect.opk zero:/media/data/apps/
